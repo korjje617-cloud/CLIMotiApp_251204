d227ec0e954a5ffde255fc6cd049474825e6dc47
korjje617 <korjje617@gmail.com>
2025-12-04 12:40:26 +0900

    add, list, exit, 명령어 외

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class App {
 
+    //스캐너 사용
     private Scanner sc;
 
     public App(Scanner sc) {
         this.sc = sc;
     }
 
+    // 실행되는 메서드
     public void run() {
         System.out.println("== motivation 실행 ==");
 
+        // add로 motivation 하나 추가할 때마다 증가
         int lastId = 1;
 
+        // 코드블록 무한 반복
         while (true) {
+
             // 명령어 입력
             System.out.print("명령어 ) ");
             String input = sc.nextLine().trim();
@@ -24,26 +29,28 @@ public class App {
             if (input.equals("add")) {
 
                 System.out.print("motivation : ");
-                String moti = sc.next();
+                String motivation = sc.nextLine();
 
                 System.out.print("source : ");
-                String sour = sc.next();
+                String source = sc.nextLine();
 
                 System.out.printf("%d번 motivation이 등록 되었습니다.\n", lastId);
                 lastId++;
 
-            } else if (input.equals("list")) {
+            } // "list" > 목록
+            else if (input.equals("list")) {
+                System.out.println("=".repeat(50));
+                System.out.println("번호     /     source     /     motivation");
 
 
-
-
-
-            } else if (input.equals("exit")) {
+            } // "exit" > 종료
+            else if (input.equals("exit")) {
                 System.out.println("== motivation 종료 ==");
                 break;
 
-            } else if (input.length() == 0) {
-                System.out.println("사용 가능 명령어\n1. add (추가하기)\n2. list (목록조회)\n3. exit (종료하기)");
+            } // 명령어 외 일괄
+            else {
+                System.out.println("[사용 가능 명령어]\n1. add (추가하기)\n2. list (목록조회)\n3. exit (종료하기)");
             }
 
         }
